8672910295703f54899460c7f89035f84db8112f
Sumit Shastri <mrsumitshastri@gmail.com>
2016-02-26 23:25:38 +0530

    Commit
    Commit

---
+++ src/com/Session1/Assignment4/Session1Assignment3.java
@@ -0,0 +1,27 @@
+package com.Session1.Assignment4;
+
+public class Session1Assignment3 {
+public static void main(String[] args) {
+	
+/*
+ * 	Problem 
+ * Assume that the integer variable x is 5 and the integer variable y is 10. Give the values of the following expressions:
+ * 	￼a. x + y * 2
+ * 	￼b. x - y + 2
+ * 	￼c. (x+y) * 2
+ * 	￼d. y % x
+ */
+	
+	int x = 5;
+	int y = 10; 
+	
+	System.out.println("x + y * 2 = "+ (x + y * 2));
+	System.out.println("x - y + 2 = "+ (x - y + 2));
+	System.out.println("(x+y) * 2 = "+ (x+y) * 2);
+	System.out.println("y % x = "+ y % x);
+	
+	
+	
+	
+}
+}
